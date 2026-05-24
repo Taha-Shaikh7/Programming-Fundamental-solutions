5343fbadf607f7f2154d5f9721e99e5c0334b93e
Muhammad Taha <ts4737076@gmail.com>
2026-05-24 17:31:13 +0500

    Add files via upload

---
+++ 2nd semester OOP labs in java/lab 5/codes/labexercise/task1.java
@@ -0,0 +1,30 @@
+public class task1 {
+class student{
+int id;
+String name;
+int marks;
+void display(){
+System.out.println("ID "+id);
+System.out.println("name "+name);
+System.out.println("marks "+marks);
+
+}}
+
+
+
+
+
+public static void main(String[] args) {
+task1.student s1=new task1().new student(); 
+s1.id = 1233;
+s1.name = "taha";
+s1.marks = 22;
+s1.display();
+
+
+
+
+
+
+}
+}
---
+++ 2nd semester OOP labs in java/lab 5/codes/labexercise/task2.java
@@ -0,0 +1,29 @@
+public class task2 {
+int id;
+String name;
+task2(int i,String n){
+id=i;
+name=n;
+}
+void display(){
+System.out.println(id);
+System.out.println(name);
+}
+
+
+public static void main(String[] args) {
+task2 s1=new task2(23, "taha");
+s1.display();
+
+
+
+
+
+
+
+
+
+
+
+}
+}
